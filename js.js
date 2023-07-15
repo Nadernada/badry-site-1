89f9743f1334f3a683b9bcb7f6ead98af0af7880
Nadernada <59936807+Nadernada@users.noreply.github.com>
2023-07-15 19:19:55 +0100

    Update js.js

--- js.js
+++ js.js
@@ -1,104 +1,41 @@
-gsap.registerPlugin(ScrollTrigger);
+window.addEventListener("DOMContentLoaded", (event) => {
 
-gsap.from(".com-border-r", {
-    scrollTrigger: {
-        trigger: ".com-main",
-        toggleActions: "restart pause resume pause",
-        start: 'top 60%',
-    },
-    duration: 1,
-     scale: 0.7,
-      x: 200,
-       y: 300,
-        ease: "fade"});
-gsap.from(".com-border-y", {
-    scrollTrigger: {
-        trigger: ".com-main",
-        toggleActions: "restart pause resume pause",
-        start: 'top 60%',
-    },
-    duration: 1,
-     scale: 0.7,
-      x: -200,
-       y: -300,
-       opacity: 0,
-        ease: "fade"});
+// album page js
 
-gsap.from(".com-main", {
-    scrollTrigger: {
-        trigger: ".com-main",
-        toggleActions: "restart pause resume pause",
-        start: 'top 60%',
-    },
-    duration: 1.5,
-     scale: 0.3,
-      opacity: 0,
-       ease: "fade"});
+      // Handle form submission
+      document.getElementById('UserInfoForm').addEventListener('submit', function(e) {
+        e.preventDefault();
+        
+        // Store form data in session storage
 
+        if (document.getElementById('20x20').checked) {
+        sessionStorage.setItem('20x20','20x20cm');
+        } else {
+          sessionStorage.setItem('20x20','false');
 
-gsap.from(".corporate-border-r", {
-    scrollTrigger: {
-        trigger: ".corporate-border-r",
-        toggleActions: "restart pause resume pause",
-        start: 'top 50%',
-    },
-    duration: 1,
-     scale: 0.7,
-      x: 200,
-       y: 300,
-        ease: "fade"});
-gsap.from(".corporate-border-y", {
-    scrollTrigger: {
-        trigger: ".corporate-border-y",
-        toggleActions: "restart pause resume pause",
-        start: 'top 50%',
-    },
-    duration: 1,
-     scale: 0.7,
-      x: -200,
-       y: -300,
-       opacity: 0,
-        ease: "fade"});
+        }
+        if (document.getElementById('30x30').checked) {
+        sessionStorage.setItem('30x30','30x30cm');
+        } else {
+          sessionStorage.setItem('30x30','false');
 
-gsap.from(".corporate-main", {
-    scrollTrigger: {
-        trigger: ".corporate-main",
-        toggleActions: "restart pause resume pause",
-        start: 'top 50%',
-    },
-    duration: 1.5,
-     scale: 0.3,
-      opacity: 0,
-       ease: "fade"});
+        }
+        if (document.getElementById('30x40').checked) {
+        sessionStorage.setItem('30x40','30x40cm');
+        } else {
+          sessionStorage.setItem('30x40','false');
 
+        }
 
+        sessionStorage.setItem('file', document.getElementById('formFileLg').value);
+        sessionStorage.setItem('name', document.getElementById('name').value);
+        sessionStorage.setItem('email', document.getElementById('email').value);
+        sessionStorage.setItem('phone', document.getElementById('phone').value);
+        sessionStorage.setItem('adresse', document.getElementById('adresse').value);
+    
+        // Redirect to next page
+        window.location.href = 'checkout.html';
+      });
 
-
-gsap.set(".img-container", {position: 'absolute', y: 300});
-
-gsap.to('.img-container', {
-    yPercent: -100,
-    stagger: 0.5,
-    scrollTrigger: {
-        trigger: ".com-main",
-        start: '.com-main 30%',
-        end: '+=1000px',
-        scrub: ".section",
-        pin: ".section"
-    },
-    opacity: 1
-})
-gsap.set(".img-container1", {position: 'absolute', y: 300});
-
-gsap.to('.img-container1', {
-    yPercent: -100,
-    stagger: 0.5,
-    scrollTrigger: {
-        trigger: ".section1",
-        start: 'top 30%',
-        end: '+=1000px',
-        scrub: ".section1",
-        pin: ".section1"
-    },
-    opacity: 1
-})
+// album page js          
+  });
