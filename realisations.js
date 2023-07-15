b47c4421643cd3ebcbdccbf1e5920ff36d6018c3
Nadernada <59936807+Nadernada@users.noreply.github.com>
2023-07-15 20:54:00 +0100

    Add files via upload

---
+++ realisations.js
@@ -0,0 +1,123 @@
+window.addEventListener("DOMContentLoaded", (event) => {
+    
+    // realisation page
+
+
+    
+    var swiper = new Swiper(".mySwiper", {
+        spaceBetween: 10,
+        slidesPerView: 5,
+        freeMode: true,
+        observer: true,
+                observeParents: true,
+        watchSlidesProgress: true,
+      });
+      
+      var swiper2 = new Swiper(".mySwiper2", {
+        observer: true,
+                observeParents: true,
+        spaceBetween: 10,
+        navigation: {
+          nextEl: ".next",
+          prevEl: ".prev",
+        },
+        thumbs: {
+          swiper: swiper,
+        },
+      });
+    
+    
+      var swiper = new Swiper(".mySwiper3", {
+        spaceBetween: 10,
+        slidesPerView: 5,
+        freeMode: true,
+        observer: true,
+                observeParents: true,
+        watchSlidesProgress: true,
+      });
+      
+      var swiper2 = new Swiper(".mySwiper4", {
+        observer: true,
+                observeParents: true,
+        spaceBetween: 10,
+        navigation: {
+          nextEl: ".next-3",
+          prevEl: ".prev-3",
+        },
+        thumbs: {
+          swiper: swiper,
+        },
+      });
+    
+      
+      var swiper = new Swiper(".mySwiper5", {
+        spaceBetween: 10,
+        slidesPerView: 5,
+        freeMode: true,
+        observer: true,
+                observeParents: true,
+        watchSlidesProgress: true,
+      });
+      
+      var swiper2 = new Swiper(".mySwiper6", {
+        observer: true,
+                observeParents: true,
+        spaceBetween: 10,
+        navigation: {
+          nextEl: ".next-5",
+          prevEl: ".prev-5",
+        },
+        thumbs: {
+          swiper: swiper,
+        },
+      });
+    
+    
+      var swiper = new Swiper(".mySwiper7", {
+        spaceBetween: 10,
+        slidesPerView: 5,
+        freeMode: true,
+        observer: true,
+                observeParents: true,
+        watchSlidesProgress: true,
+      });
+      
+      var swiper2 = new Swiper(".mySwiper8", {
+        observer: true,
+                observeParents: true,
+        spaceBetween: 10,
+        navigation: {
+          nextEl: ".next-7",
+          prevEl: ".prev-7",
+        },
+        thumbs: {
+          swiper: swiper,
+        },
+      });
+    
+    
+      var swiper = new Swiper(".mySwiper9", {
+        spaceBetween: 10,
+        slidesPerView: 5,
+        freeMode: true,
+        observer: true,
+                observeParents: true,
+        watchSlidesProgress: true,
+      });
+      
+      var swiper2 = new Swiper(".mySwiper10", {
+        observer: true,
+                observeParents: true,
+        spaceBetween: 10,
+        navigation: {
+          nextEl: ".next-9",
+          prevEl: ".prev-9",
+        },
+        thumbs: {
+          swiper: swiper,
+        },
+      });
+
+    
+      // realisations page
+      });
