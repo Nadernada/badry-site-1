c5a6048e6fbb9eb11d77f65f307972356040e38f
Nadernada <59936807+Nadernada@users.noreply.github.com>
2023-07-27 13:46:53 +0100

    Update js.js

--- js.js
+++ js.js
@@ -3,32 +3,25 @@ window.addEventListener("DOMContentLoaded", (event) => {
 // album page js
 
       // Handle form submission
+      // Store form data in session storage
       document.getElementById('UserInfoForm').addEventListener('submit', function(e) {
         e.preventDefault();
-        
-        // Store form data in session storage
 
-        if (document.getElementById('20x20').checked) {
-        sessionStorage.setItem('20x20','20x20cm');
-        } else {
-          sessionStorage.setItem('20x20','false');
+        var form = document.getElementById("UserInfoForm");
+      var selectedOptions = [];
 
+      // Loop through each checkbox
+      var checkboxes = form.getElementsByTagName("input");
+      for (var i = 0; i < checkboxes.length; i++) {
+        if (checkboxes[i].type === "checkbox" && checkboxes[i].checked) {
+          selectedOptions.push(checkboxes[i].value);
+          sessionStorage.setItem('sizes', selectedOptions);
         }
-        if (document.getElementById('30x30').checked) {
-        sessionStorage.setItem('30x30','30x30cm');
-        } else {
-          sessionStorage.setItem('30x30','false');
-
-        }
-        if (document.getElementById('30x40').checked) {
-        sessionStorage.setItem('30x40','30x40cm');
-        } else {
-          sessionStorage.setItem('30x40','false');
-
-        }
+      }
       if(document.getElementById('cover').checked) {
         sessionStorage.setItem('coverOption', '+ Couverture')
   }
+        
         sessionStorage.setItem('file', document.getElementById('formFileLg').value);
         sessionStorage.setItem('name', document.getElementById('name').value);
         sessionStorage.setItem('email', document.getElementById('email').value);
@@ -36,8 +29,10 @@ window.addEventListener("DOMContentLoaded", (event) => {
         sessionStorage.setItem('adresse', document.getElementById('adresse').value);
     
         // Redirect to next page
-        window.location.href = 'checkout.html';
+        window.location.assign('checkout.html');
       });
 
-// album page js          
+// album page js   
+
+
   });
